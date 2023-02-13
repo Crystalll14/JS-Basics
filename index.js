455217f58926051e3d363de3ea32ea268fe2ec40
crystalll14 <nkemeh.crystal@stu.cu.ed.ng>
2023-02-13 10:59:42 +0100

    added files

---
+++ index.js
@@ -0,0 +1,15 @@
+function  multiply (num1,num2) {
+    return num1 * num2;
+}
+
+console.log(multiply(3,15))
+
+let FirstName = 'Crystal';
+let LastName = 'Nkemeh';
+let Stuterntrack = 'BackEnd Development';
+let Skills = 'Basketball';
+let Favcolor = 'Purple';
+let CountryIndepe = '1960';
+
+console.log('My name is ' + FirstName + ' ' + LastName + '. My favorite color is ' + Favcolor + ' and one of my many skills is ' + Skills
++ '. My Country got her Independence in ' + CountryIndepe + '. I am currently enrolled in the ' + Stuterntrack + ' track at Stutern.')
